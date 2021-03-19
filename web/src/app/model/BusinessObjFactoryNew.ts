618a8b9ef4da0298dca47bf0c284f1b11247de25
Rajesh <mahisfashionparadise@gmail.com>
2021-03-19 10:11:44 -0400

    latest change added in  db

---
+++ web/src/app/model/BusinessObjFactoryNew.ts
@@ -0,0 +1,17 @@
+import { Customer } from './CustomerNew';
+import { Dealer } from './DealerNew';
+import { OrderSummary } from './OrderSummary';
+import { Product } from './ProductNew';
+import { PurchaseDetails } from './PurchaseDetails';
+export class BusinessObjFactory {
+    static getBusinessObjInstance(busObjName) {
+        switch (busObjName.toLowerCase()) {
+            case 'dealer' : return new Dealer();
+            case 'customer' : return new Customer();
+            case 'product' : return new Product();
+            case 'purchasedetails' : return new PurchaseDetails();
+            case 'ordersummary' : return new OrderSummary();
+        }
+        return null;
+    }
+}
